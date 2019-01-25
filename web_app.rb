f9c5819ffc70ab4103b84c9ac03f887d2af814d2
Ellen Marie Dash <me@duckie.co>
2019-01-25 17:36:13 -0500

    *shrugs forever*

--- web_app.rb
+++ web_app.rb
@@ -8,7 +8,7 @@ def css(fg, bg)
   <<~EOF
     body {
       padding: 1em;
-      font-family: sans-serif;
+      font-family: monospace;
       max-width: 60ch;
       margin: auto;
       font-size: 120%;
@@ -16,9 +16,10 @@ def css(fg, bg)
       background: #{bg};
     }
 
+    a { color: inherit; }
     input, textarea {
       padding: 1em;
-      color:inherit;
+      color: inherit;
     }
     input[type=text], textarea {
       background: rgba(0, 0, 0, 0.2);
@@ -34,25 +35,30 @@ def css(fg, bg)
 end
 
 get "/" do
-  if params[:color]
-    color = ColorPair::RGB.parse(params[:color])
+  if params[:fg]
+    raw_fg = ColorPair::RGB.parse(params[:fg])
   else
-    color = ColorPair::RGB.random
+    raw_fg = ColorPair::RGB.random
   end
 
-  raw_fg, raw_bg = ColorPair.pair_from(color)
+  if params[:bg]
+    raw_bg = ColorPair::RGB.parse(params[:bg])
+  else
+    raw_fg, raw_bg = ColorPair.pair_from(raw_fg)
+  end
+
   fg, bg = [raw_fg, raw_bg].map(&:to_css)
 
 <<-EOF
 <style>
 #{css(fg, bg)}
 </style>
-<p>hello, world!</p>
+<p><a href="/?fg=#{fg}&bg=#{bg}">Permalink.</a></p>
 <form>
-<label>Foreground: <input name="color" type="text" value="#{fg}"></label>
-<input type="submit" value="Submit">
+<p><label>Foreground: <input name="fg" type="text" value="#{fg}"></label></p>
+<p><label>Background: <input name="bg" type="text" value="#{bg}"></label></p>
+<p><input type="submit" value="Submit"></p>
 </form>
-<p>Background: #{bg}</p>
 <p>Contrast ratio: #{ColorPair.contrast_ratio(raw_fg, raw_bg).round(2)}:1</p>
 
 <!-- bullshit it and hard-code the height, since we know the number of rows -->
